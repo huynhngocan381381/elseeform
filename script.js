2ec2a3934aacc087f8a07c6f1da5b90032e4ae1b
Huynh Ngoc An <huynhngocan381381@gmail.com>
2023-05-03 20:51:42 +0700

    Change bvs to code in login page

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const form = document.getElementById("login-form");
 const usernameInput = document.getElementById("username");
 const passwordInput = document.getElementById("password");
-const bvsInput = document.getElementById("bvs");
+const codeInput = document.getElementById("code");
 const errorMessage = document.getElementById("error-message");
 
 form.addEventListener("submit", (event) => {
@@ -9,9 +9,9 @@ form.addEventListener("submit", (event) => {
 
     const username = usernameInput.value.trim();
     const password = passwordInput.value.trim();
-    const bvs = bvsInput.value.trim();
+    const code = codeInput.value.trim();
 
-    if (username === "elsee" && password === "123456" && bvs === "BVTKH") {
+    if (username === "elsee" && password === "123456" && code === "BVTKH") {
         window.open('./form.html');
     } else {
         errorMessage.textContent =
